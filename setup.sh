56bed2643ecabe68a186b28dc4c98ecaf530a1db
ajaidan0 <ajaidan0@gmail.com>
2020-07-19 22:00:29 -0400

    add respring and ssh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,15 @@ cd $HOME
 
 sudo apt install fakeroot curl wget
 
-git clone https://github.com/ajaidan0/TheosScript.git
-
 cd $HOME/TheosScript
 
 sudo chmod +x ./theos
 
-sudo ln ./theos /usr/bin
+sudo ln ./theos /usr/local/bin
+
+read -p "Enter your iPhone's IP Address: " IP
+echo "export THEOS_DEVICE_IP=${IP}" >> ~/.bashrc
+echo "export THEOS_DEVICE_IP=${IP}" >> ~/.profile
+echo ""
+
+echo "The THEOS Command has been installed. Please restart your terminal to allow the variables to take effect."
