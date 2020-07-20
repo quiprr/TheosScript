d25cef52ce72532cf9e8915769fafa17de1392a9
ajaidan0 <37126748+ajaidan0@users.noreply.github.com>
2020-07-19 22:01:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ cd $HOME/TheosScript
 sudo chmod +x ./theos
 
 sudo ln ./theos /usr/local/bin
-<<<<<<< HEAD
 
 read -p "Enter your iPhone's IP Address: " IP
 echo "export THEOS_DEVICE_IP=${IP}" >> ~/.bashrc
@@ -17,5 +16,3 @@ echo "export THEOS_DEVICE_IP=${IP}" >> ~/.profile
 echo ""
 
 echo "The THEOS Command has been installed. Please restart your terminal to allow the variables to take effect."
-=======
->>>>>>> 9c50f7c5cd758a057e2b6108bd16d1cb6dad6106
