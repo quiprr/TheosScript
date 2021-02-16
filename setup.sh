5e10b4597935dcd912bedf2c21226435388c0e6a
Monotrix <ajaidan0@gmail.com>
2021-02-16 01:23:38 -0500

    MASSIVE UPDATE (Better Mac support, setup.sh fixes, etc etc)

--- setup.sh
+++ setup.sh
@@ -42,64 +42,70 @@ command -v python >/dev/null 2>&1 || need+="python "
 command -v perl >/dev/null 2>&1 || need+="perl"
 
 macosInstall() {
-    command -v ldid >/dev/null 2>&1 || need+="ldid "
-    command -v xz >/dev/null 2>&1 || need+="xz"
-    if [ "$need" != "" ]; then
-      read -p "Using Brew To Install Dependencies (${need}). Press Enter to Continue." || exit 1
-      brew install $need
-      echo -e "${GR}All dependencies installed successfully.${NC}"
-    fi
+  command -v ldid >/dev/null 2>&1 || need+="ldid "
+  command -v xz >/dev/null 2>&1 || need+="xz"
+  if [ "$need" != "" ]; then
+    read -p "Using Brew To Install Dependencies (${need}). Press Enter to Continue." || exit 1
+    brew install $need
+    echo -e "${GR}All dependencies installed successfully.${NC}"
+  fi
 }
 
 linuxInstall() {
-    command -v clang-6.0 >/dev/null 2>&1 || need+="clang-6.0 "
-    if [ "$need" != "" ]; then
-      read -p "Installing Dependencies (${need}). Press Enter to Continue." || exit 1
-      if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $need || failedinstall=1
-       elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $need || failedinstall=1
-       elif [ -x "$(command -v dnf)" ];     then sudo dnf install $need || failedinstall=1
-       elif [ -x "$(command -v zypper)" ];  then sudo zypper install $need || failedinstall=1
-      else failedinstall=1;
-      fi
-      if [ $failedinstall == 1 ]; then
-        echo -e "${BAD}You need to manually install:${NC} $need">&2; 
-      else
-        echo -e "${GR}All dependencies installed successfully.${NC}"
-      fi
+  command -v fakeroot >/dev/null 2>&1 || need+="fakeroot "
+  command -v clang-6.0 >/dev/null 2>&1 || need+="clang-6.0 "
+  if [ "$need" != "" ]; then
+    read -p "Installing Dependencies (${need}). Press Enter to Continue." || exit 1
+    if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $need || failedinstall=1
+      elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $need || failedinstall=1
+      elif [ -x "$(command -v dnf)" ];     then sudo dnf install $need || failedinstall=1
+      elif [ -x "$(command -v zypper)" ];  then sudo zypper install $need || failedinstall=1
+    else failedinstall=1;
     fi
+    if [ $failedinstall == 1 ]; then
+      echo -e "${BAD}You need to manually install:${NC} $need">&2; 
+    else
+      echo -e "${GR}All dependencies installed successfully.${NC}"
+    fi
+  fi
 }
 
 script() {
-    if [ "$(uname -s)" == "Darwin" ]; then 
-        if [ "$(uname -p)" == "arm" ] || [ "$(uname -p)" == "arm64" ]; then echo "This script is incompatable with iOS." exit 1
-        else macosInstall
-        fi
-    else linuxInstall
+  if [ "$(uname -s)" == "Darwin" ]; then 
+    if [ "$arch" == "arm" ] || [ "$arch" == "arm64" ]; then
+	    if [ "${USER}" == "mobile" ]; then
+	      echo "This script does not support iOS."
+        exit 1
+	    else
+        macosInstall
+	    fi
     fi
-    cd $HOME
+  else linuxInstall
+  fi
+  cd $HOME
 
-    if [ "$commandinstalled" != "false" ]; then
-        if [ "$(uname -s)" == "Darwin" ]; then
-          sudo rm /usr/local/bin/theos
-        else
-          sudo rm /usr/bin/theos
-        fi
-        rm -rf theosscript
-    fi
+  if [ "$commandinstalled" != "false" ]; then
+      if [ "$(uname -s)" == "Darwin" ]; then
+        sudo rm /usr/local/bin/theos
+      else
+        sudo rm /usr/bin/theos
+      fi
+      rm -rf theosscript
+  fi
     
-    git clone https://github.com/monotrix/theosscript
+  git clone https://github.com/monotrix/theosscript
 
-    cd $HOME/theosscript
+  cd $HOME/theosscript
 
-    sudo chmod +x ./theos
+  sudo chmod +x ./theos
 
-    if [ "$(uname -s)" == "Darwin" ]; then
-      sudo ln -s $HOME/theosscript/theos /usr/local/bin/theos
-    else sudo ln -s $HOME/theosscript/theos /usr/bin/theos
-    fi 
+  if [ "$(uname -s)" == "Darwin" ]; then
+    sudo ln -s $HOME/theosscript/theos /usr/local/bin/theos
+  else sudo ln -s $HOME/theosscript/theos /usr/bin/theos
+  fi 
 
-    cd $previousdir
-    echo "The THEOS Command has been installed."
+  cd $previousdir
+  echo "The THEOS Command has been installed."
 }
 
 script
